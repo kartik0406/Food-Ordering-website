e7d312963bbb093288bf0e6cbde07e6f0b80a247
Kartik Khanna <66087752+kartik0406@users.noreply.github.com>
2020-07-07 00:50:37 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,8 @@
 self.addEventListener('fetch',() => console.log("fetch"));
+window.addEventListener("beforeinstallprompt", function(e) { 
+  // log the platforms provided as options in an install prompt 
+  console.log(e.platforms); // e.g., ["web", "android", "windows"] 
+  e.userChoice.then(function(choiceResult) { 
+    console.log(choiceResult.outcome); // either "accepted" or "dismissed"
+  }, handleError); 
+});
