4700e29e2704e2efc9eb6d50ea46c0f183da95cd
Kartik Khanna <66087752+kartik0406@users.noreply.github.com>
2020-07-07 09:58:49 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,19 @@
-self.addEventListener('fetch', function(event) {});
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.prevent.default();
+  deferredPrompt=e;
+  btnAdd.style.display='block';
+});
+btnAdd.addeventListner('click, (e) => {
+       deferredPrompt.prompt();
+    deferredPrompt.userchoice.then((choiceResult) => {
+      
+      if(choice.result==='accepted') {
+        console.log('user A2H accepted');
+      }
+      deferredPrompt=null;
+    });
+});
+window.addEventListener('appinstalled',(evt) => {
+  app.logEvent('a2hs','installed');
+});
